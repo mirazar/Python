6f893de40dd6554810274814a07f8f8e0c31a57d
Viktor Zemlyanov <mirazar@yandex.ru>
2021-04-06 02:15:42 +0300

    lesson-2

---
+++ 1.py
@@ -0,0 +1,3 @@
+a = [34, 'hello', True, None, 34.4, ['Hello world', 56.6]]
+for i in a:
+    print(type(i))
---
+++ 4.py
@@ -0,0 +1,4 @@
+a = input('Введите предлжение').split()
+for i, el in enumerate(a):
+    i += 1
+    print(i, el[:10])
